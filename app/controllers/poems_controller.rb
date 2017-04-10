92491e2525e18efb118dc24840a47fb16a3ce7e5
越智 昭文 <akifumi.ochi@gmail.com>
2017-04-10 14:48:10 +0900

    API基礎2

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,14 +1,13 @@
 class PoemsController < ApplicationController
+
   def index
     @poems = Poem.all
     render json: @poems
   end
 
   def show
-    @poems = Poem.find(params:[id])
-    render json: @poems
+    @poem = Poem.find(params:[id])
+    render json: @poem
   end
 
-
-  
 end
