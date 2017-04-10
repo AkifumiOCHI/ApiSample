93d87c1ba39be8d0326dcb49b34ba00d855f65b1
越智 昭文 <akifumi.ochi@gmail.com>
2017-04-10 14:00:19 +0900

    API課題2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :blogs, only: [:index]
 
-  resources :poems, only: [:index]
+  resources :poems, only: [:index, :show]
+
+  root 'top#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
